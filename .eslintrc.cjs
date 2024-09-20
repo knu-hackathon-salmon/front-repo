fd7ae230672b34aa4f0aa00b8eec64ea242f62c0
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-20 14:24:27 +0900

    refactor: eslint 재설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,8 @@ module.exports = {
 
     rules: {
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-unused-vars": "off"
         "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-explicit-any": "warn",
