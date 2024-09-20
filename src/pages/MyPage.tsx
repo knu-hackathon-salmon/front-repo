b7eac9069612d5d13d3a29028a05c78d59edfdfa
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-20 16:51:24 +0900

    feat: MyPage 탭으로 구매목록/채팅방 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 import { styled } from "styled-components";
 
 import { Text } from "@/components/common/Text";
+import { FoodList } from "@/components/features/FoodItem/FoodList";
+
+interface TabButtonProps {
+    isActive: boolean;
+}
 
 export default function MyPage() {
+    {
+        /*유저정보를 받아서 구매자인지 판매자인지 구분하는 코드*/
+    }
+    const [activeTab, setActiveTab] = useState("first");
     return (
         <>
             <InfoWrapper>
@@ -21,9 +32,17 @@ export default function MyPage() {
                         @gyul
                     </Text>
                 </UserInfo>
-                <Btn>구매 목록</Btn>
             </InfoWrapper>
-            <ListWrapper>{/* <PurchaseList /> */}</ListWrapper>
+            <TabMenu>
+                <TabButton isActive={activeTab === "first"} onClick={() => setActiveTab("first")}>
+                    구매목록
+                </TabButton>
+                <TabButton isActive={activeTab === "chat"} onClick={() => setActiveTab("chat")}>
+                    채팅방
+                </TabButton>
+            </TabMenu>
+
+            <ListWrapper>{activeTab === "first" ? <FoodList isH={true} /> : <FoodList isH={true} />}</ListWrapper>
         </>
     );
 }
@@ -31,7 +50,7 @@ export default function MyPage() {
 const InfoWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    width: 100%;
+    width: 90%;
     align-items: center;
     gap: 20px;
 `;
@@ -58,14 +77,26 @@ const Image = styled.img`
     border-radius: 50%;
     aspect-ratio: 1 / 1;
 `;
-const Btn = styled.button`
-    border: 1px solid #126245;
-    background: none;
-    color: #126245;
-    font-weight: bold;
-    padding: 6px 16px;
-    font-size: 14px;
-    border-radius: 10px;
+
+const TabMenu = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    margin: 50px 0px 20px;
+    width: 90%;
 `;
 
-const ListWrapper = styled.div``;
+const TabButton = styled.button<TabButtonProps>`
+    border-bottom: ${(props) => (props.isActive ? "2px solid #126245" : "1px solid #C4C4C4")};
+    background: none;
+    color: ${(props) => (props.isActive ? "#126245" : "#C4C4C4")};
+    font-weight: bold;
+    padding: 18px;
+    font-size: 14px;
+    cursor: pointer;
+    width: 100%;
+`;
+
+const ListWrapper = styled.div`
+    margin-top: 20px;
+    width: 90%;
+`;
