ca6df66936f7e3ad172f3d76dda7c49e4d2ee9f2
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-20 17:23:24 +0900

    design: 글로벌 스타일 수정

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    body {
+    body, p, span {
         margin: 0;
         padding: 0;
         font-family: "Pretendard", sans-serif;
