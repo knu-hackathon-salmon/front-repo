77ce398e051e4a91c558352d70b38a81d480326b
Hyoeunkh <lhe4528@knu.ac.kr>
2024-09-21 17:42:30 +0900

    fix: 무한 렌더링 수정

--- src/provider/Auth/index.tsx
+++ src/provider/Auth/index.tsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
             });
         }
         setIsReady(true);
-    }, [currentAuthToken]);
+    }, []);
 
     if (!isReady) return <></>;
     return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
